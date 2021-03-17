0244e3b415115a0300343fa2ddffca7776edc57c
NITRO5 <gansimarina@gmail.com>
2021-03-17 09:47:08 +0545

    BMI

---
+++ pythonlab1/lab 1/6.py
@@ -0,0 +1,8 @@
+#solve each of the following problems using python  scripts.make sure you use appropriate variable  names and comments
+#when there is a final answer have python print it to the screen.
+#A person's body index (BMI) is defind as
+#BMI = mass in kg(height in m)^2
+mass = float (input ("enter the mass f person in kg:"))
+height =float (input ("enter the height of a program  in meter:"))
+BMI=(mass/ (height * height ))
+print (f"the BMI index of a program is {BMI}")
