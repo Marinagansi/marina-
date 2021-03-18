f99e7e804bf06d014149eb4d3b0074d28fd88429
NITRO5 <gansimarina@gmail.com>
2021-03-18 09:33:41 +0545

    'q.no10

--- pythonlab1/lab 1/10.py
+++ pythonlab1/lab 1/10.py
@@ -1,8 +1,11 @@
 #write a python program to convert seconds to day,hour,minutes and seconds
-seconds=int(input("enter the time"))
+second=int(input("enter the time"))
 #into minute
-minute= seconds// 60
-print(f"{seconds} secis equal to {minute} min")
-#into hour
-hour= seconds//120
-print("hour is", hour)
+minute= second/60
+hour= (second/60)/60
+day=(((second/60)/60)/24)
+remain_second= second * 60
+print(f'{second} sec is equal to {minute} min')
+print(f"{second} sec is eual to {hour}")
+print(f"{second} sec is equal to {day} day")
+print (f" {remain_second} is remain seconds")
