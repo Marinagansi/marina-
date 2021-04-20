28fe7b31d79acbdc3edc2d55afe6b554e190ac82
NITRO5 <gansimarina@gmail.com>
2021-04-20 14:41:24 +0545

    assignment_calculator

--- eventanoucement/eas.py
+++ eventanoucement/eas.py
@@ -1,2 +1,9 @@
-from thinker import*
+from tkinter import*
 EAS=Tk()
+EAS.title("Event_Announcement")
+#creating label widget
+mylabel=Label(EAS,text="tittle")
+mylabel1=Label(EAS,text="categories")
+mylabel.grid(row=0,column=0)
+mylabel1.grid(row=0,column=4)
+EAS.mainloop()
